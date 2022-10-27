f8ef4c8a5fbd985b42e68278151c7e93e25608ab
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-10-27 17:08:38 +0200

    Update and rename support to support.py

--- support.py
+++ support.py
@@ -1,3 +1,4 @@
 https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login-ru   Create Registration
 https://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/                            Create Search Form
 https://www.youtube.com/watch?v=RV5o2-UB_dw                                                                  OST AOT
+https://pythonru.com/uroki/18-autentifikacija-vo-flask                                                        Create Registration
