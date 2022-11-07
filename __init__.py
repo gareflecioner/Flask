02b6077f302e7fe2347ff601928905826f0b42fd
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-07 18:04:58 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,30 @@
+import math
+import sqlite3
+import time
+from flask import Flask, render_template, request, redirect, url_for, flash, g, session
+from flask_bootstrap import Bootstrap
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
+
+
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///base.db"
+app.config["SECRET_KEY"]="secret_key_i_love-attack-on-titan"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+db=SQLAlchemy(app)
+bootstrap=Bootstrap(app)
+login_manager=LoginManager(app)
+login_manager.login_view="sing_in"
+#init_app(app)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=-True)
