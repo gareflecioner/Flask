9e27fe4ba03b3f7f408f044e9adbf35baa536b91
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-07 18:03:26 +0300

    Update support.py

--- support.py
+++ support.py
@@ -2,3 +2,4 @@ https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-yo
 https://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/                            Create Search Form
 https://www.youtube.com/watch?v=RV5o2-UB_dw                                                                  OST AOT
 https://pythonru.com/uroki/18-autentifikacija-vo-flask                                                        Create Registration
+https://www.youtube.com/watch?v=U44qKaKpAMk                                                                   жесть
