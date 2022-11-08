71c4321514a16c9859b211577fefca928b62c80f
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-08 16:41:54 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,7 @@
-import math
-import sqlite3
-import time
-from flask import Flask, render_template, request, redirect, url_for, flash, g, session
-from flask_bootstrap import Bootstrap
-from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash,check_password_hash
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
-
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app=Flask(__name__)
@@ -19,8 +10,8 @@ app.config["SECRET_KEY"]="secret_key_i_love-attack-on-titan"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
 db=SQLAlchemy(app)
 bootstrap=Bootstrap(app)
-login_manager=LoginManager(app)
-login_manager.login_view="sing_in"
+login=LoginManager(app)
+login.login_view="sing_in"
 #init_app(app)
 
 
@@ -28,3 +19,5 @@ login_manager.login_view="sing_in"
 
 if __name__ == "__main__":
     app.run(debug=-True)
+
+    from app import routes, models
