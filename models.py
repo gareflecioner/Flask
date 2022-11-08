cceae0a4bbfa6be2e6debb6902bcb8c4da1d6bd8
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-08 16:38:18 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,14 +2,12 @@ import math
 import sqlite3
 import time
 from flask import Flask, render_template, request, redirect, url_for, flash, g, session
-from flask_bootstrap import Bootstrap
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
 
 @login_manager.user_loader
 def load_user(id):
@@ -23,14 +21,16 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(50), nullable=False)
     Created = db.Column(db.DateTime, default=datetime.utcnow)
     
+     def __repr__(self):
+        return '<User %r>' % self.id
+    
     def set_password(self,password):
         self.password_hash=generate_password_hash(password)
         
     def check_password(self,password):
         return check_password_hash(self.password.hash,password)
 
-    def __repr__(self):
-        return '<User %r>' % self.id
+ 
     
 #db with wishes
 class registration(db.Model):
