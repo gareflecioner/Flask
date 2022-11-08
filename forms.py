395b8a67a53304fe97ebc0aa7b2df902bdb2b2cb
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-08 16:53:39 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
     TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
     Length
+from models import User
 
 class RegistrationForm(FlaskForm):
     name=StringField('name',validators=[DataRequired()])
