a5948c523b77a91ca59827209fe13148564b4e7d
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-08 16:34:40 +0300

    Update models.py

--- models.py
+++ models.py
@@ -65,31 +65,3 @@ class papers(db.Model):
     def __repr__(self):
         return '<papers %r>' % self.id
 
-class RegistrationForm(FlaskForm):
-    name=StringField('name',validators=[DataRequired()])
-    email=StringField('email',validators=[DataRequired(),Email()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    password2=PasswordField('Repeat Password',validators=[DataRequired(),EqualTo('password')])
-    submit=SubmitField('Register')
-
-    def validate_name(self,name):
-        user=User.query.filter_by(name=name.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different name')
-
-    def validate_email(self,email):
-        user=User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different e-mail')
-
-class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me=BooleanField("Remember me")
-    submit=SubmitField("Sing in")
-
-class FeedForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), Email()])
-    feed=StringField("feed", validators=[DataRequired()])
-    submit=SubmitField("Sing in")
