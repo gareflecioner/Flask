b6349144b02f9b03891ee49c5e2ad4e82d8eb84f
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-08 16:34:20 +0300

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+    TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
+    Length
+
+class RegistrationForm(FlaskForm):
+    name=StringField('name',validators=[DataRequired()])
+    email=StringField('email',validators=[DataRequired(),Email()])
+    password=PasswordField('Password',validators=[DataRequired()])
+    password2=PasswordField('Repeat Password',validators=[DataRequired(),EqualTo('password')])
+    submit=SubmitField('Register')
+
+    def validate_name(self,name):
+        user=User.query.filter_by(name=name.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different name')
+
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different e-mail')
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me=BooleanField("Remember me")
+    submit=SubmitField("Sing in")
+
+class FeedForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    feed=StringField("feed", validators=[DataRequired()])
+    submit=SubmitField("Sing in")
