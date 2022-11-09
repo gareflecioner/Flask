41bc8bf0de5a121232aeb4ecc383e6e5a26d3a4b
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-09 17:32:34 +0300

    Update support.py

--- support.py
+++ support.py
@@ -3,3 +3,4 @@ https://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form
 https://www.youtube.com/watch?v=RV5o2-UB_dw                                                                  OST AOT
 https://pythonru.com/uroki/18-autentifikacija-vo-flask                                                        Create Registration
 https://www.youtube.com/watch?v=U44qKaKpAMk                                                                   жесть
+https://www.youtube.com/watch?v=fNGMsBI0R30                                                                    elasticsearch
