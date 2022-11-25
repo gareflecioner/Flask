cca67fe50a3bb23dd5dda5a105c8c4cf06f3bde5
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-11-25 23:46:03 +0300

    25.11

--- parcingpapers.py
+++ parcingpapers.py
@@ -5,9 +5,9 @@ import sqlite3
 url='https://www.womanandhome.com/life/books/the-25-best-books-of-all-time-for-your-must-read-listfrom-popular-fiction-to-classic-novels/'
 response=requests.get(url)
 soup=BeautifulSoup(response.content,"lxml")
-#записывается в бд корневой папки
+
 def add(title,description,market,price):
-    conn=sqlite3.connect("base.db")
+    conn=sqlite3.connect("instance/again.db")
     cur = conn.cursor()
     cur.execute("""
     INSERT INTO papers (title,description,market,price)
@@ -20,11 +20,22 @@ item=soup.find(id="article-body")
 nazvanie=item.find_all("div",class_="featured_product_details_wrapper")
 cena=item.find_all("div",class_="featured_product_block featured_block_hero")
 
-for block in nazvanie:
-    title=block.find('div',class_="featured__title").text.strip()
-    description=block.find("div",class_="subtitle__description").text.strip()
-    print(title)
-    print(description)
+#title = nazvanie.find_all('div', class_="featured__title").text.strip()
+#description = nazvanie.find_all("div", class_="subtitle__description").text.strip()
+#market=cena.find_all('a',class_="hawk-affiliate-link-merchantlink-label").text.strip()
+#price = cena.find_all('div', class_="hawk-display-price-price")
+
+#print(title)
+#print(description)
+#print(market)
+#print(price)
+
+
+#for block in nazvanie:
+    #title=block.find('div',class_="featured__title").text.strip()
+    #description=block.find("div",class_="subtitle__description").text.strip()
+    #print(title)
+    #print(description)
     #add(title,description)
 #for i in cena:
     #market=i.find('a',class_="hawk-affiliate-link-merchantlink-label").text.strip()
