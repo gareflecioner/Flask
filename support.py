0e42b7ae021044e15f89f1682cd6eeefdf04579d
Aleksey Shishkin <112956739+gareflecioner@users.noreply.github.com>
2022-12-06 17:56:27 +0300

    Update support.py

--- support.py
+++ support.py
@@ -1,20 +1,10 @@
-https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login-ru   Create Registration
-https://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/                            Create Search Form
-https://www.youtube.com/watch?v=RV5o2-UB_dw                                                                  OST AOT
-https://pythonru.com/uroki/18-autentifikacija-vo-flask                                                        Create Registration
-https://www.youtube.com/watch?v=U44qKaKpAMk                                                                   жесть
-https://www.youtube.com/watch?v=fNGMsBI0R30                                                                    elasticsearch
-
-https://www.womanandhome.com/life/books/best-books-2021/
 
 https://learn.javascript.ru
   https://developer.mozilla.org/ru/docs/Web/JavaScript/Language_Overview#другие_типы
   
-  
 https://ru.reactjs.org/tutorial/tutorial.html
 https://ru.reactjs.org/docs/getting-started.html
   https://habr.com/ru/company/ruvds/blog/428077/
-  
   
 https://habr.com/ru/post/450282/
 search engine tf idf
@@ -22,4 +12,5 @@ https://habr.com/ru/company/ruvds/blog/428077/
 
   https://www.youtube.com/watch?v=Iz1NvqG7wTc
     
-    https://habr.com/ru/post/490754/
+    https://habr.com/ru/post/490754/ vscode польза
+      https://www.youtube.com/watch?v=-k8w0x-rp80 установка
